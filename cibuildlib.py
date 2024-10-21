830924ce00daa2e29f90492890b0a39cd832c2a4
root <16307130148@fudan.edu.cn>
2024-10-21 19:42:05 +0800

    .

--- cibuildlib.py
+++ cibuildlib.py
@@ -1,41 +1,55 @@
-import sys,subprocess,shutil,os
+import sys, subprocess, shutil, os
+
 print(sys.platform)
+
+
 def listdir():
-    for f in os.walk('.'):
-        _dir,_,_fs=f
+    for f in os.walk("."):
+        _dir, _, _fs = f
         for _f in _fs:
-            print(os.path.abspath(os.path.join(_dir,_f)))
-if sys.argv[1]=='release':
-    os.makedirs('./wheel')
+            print(os.path.abspath(os.path.join(_dir, _f)))
+
+
+if len(sys.argv) >= 2 and sys.argv[1] == "release":
+    os.makedirs("./wheel", exist_ok=True)
     listdir()
-    filepath=os.path.dirname(__file__)
+    filepath = os.path.dirname(__file__)
     os.chdir(filepath)
-    copy=lambda f,d:os.makedirs(d) or shutil.copy(f,d)
-    copy('./build/windows-2019x86/library/webview.dll','./webviewpy/platform/win32/x86')
-    copy('./build/windows-2019x64/library/webview.dll','./webviewpy/platform/win32/x64')
-    #copy('./build/ubuntu-20.04x86/library/libwebview.so','./webviewpy/platform/linux/x86')
-    copy('./build/ubuntu-20.04x64/library/libwebview.so','./webviewpy/platform/linux/x64')
-    #copy('./build/macos-11x86/library/libwebview.dylib','./webviewpy/platform/darwin/x86')
-    copy('./build/macos-11x64/library/libwebview.dylib','./webviewpy/platform/darwin/x64')
-    os.system('python -m build')
+    copy = lambda f, d: os.makedirs(d) or shutil.copy(f, d)
+    copy(
+        "./build/x86/bin/webview.dll", "./webviewpy/platform/win32/x86"
+    )
+    copy(
+        "./build/x64/bin/webview.dll", "./webviewpy/platform/win32/x64"
+    )
+    # copy('./build/ubuntu-20.04x86/library/libwebview.so','./webviewpy/platform/linux/x86')
+    copy(
+        "./build/x64/bin/libwebview.so",
+        "./webviewpy/platform/linux/x64",
+    )
+    # copy('./build/macos-11x86/library/libwebview.dylib','./webviewpy/platform/darwin/x86')
+    copy(
+        "./build/x64/bin/libwebview.dylib",
+        "./webviewpy/platform/darwin/x64",
+    )
+    os.system("python -m build")
     listdir()
     exit()
 
-arch=sys.argv[1]
-curr=os.getcwd()
+curr = os.getcwd()
 print(curr)
-os.system("git clone https://github.com/webview/webview")
-os.chdir(f'{curr}/webview')
-os.makedirs(f'{curr}/platform/{sys.platform}')
-os.makedirs(f'{curr}/platform/{sys.platform}/x86')
-os.makedirs(f'{curr}/platform/{sys.platform}/x64')
-if sys.platform=='win32':
-    subprocess.run(f"cmd /c set TARGET_ARCH={arch} & call script/build.bat")
-    #shutil.move(f"{curr}/webview/build/library/webview.dll", f"{curr}/platform/{sys.platform}/x86")
-    #subprocess.run(f"cmd /c set TARGET_ARCH=x64 & call script/build.bat")
-    #shutil.move(f"{curr}/webview/build/library/webview.dll", f"{curr}/platform/{sys.platform}/x64")
-elif sys.platform=='linux':
-    os.system('''
+os.makedirs(f"{curr}/platform/{sys.platform}", exist_ok=True)
+os.makedirs(f"{curr}/platform/{sys.platform}/x86", exist_ok=True)
+os.makedirs(f"{curr}/platform/{sys.platform}/x64", exist_ok=True)
+if sys.platform == "win32":
+    os.system("cmake -A win32 -T host=x86 -B ./build/x86")
+    os.system("cmake --build ./build/x86 --config Release --target ALL_BUILD")
+
+    os.system("cmake -A x64 -T host=x64 -B ./build/x64")
+    os.system("cmake --build ./build/x64 --config Release --target ALL_BUILD")
+elif sys.platform == "linux":
+    os.system(
+        """
 sudo update-alternatives --force --install /usr/bin/gcc gcc /usr/bin/gcc-12 1200 --slave /usr/bin/g++ g++ /usr/bin/g++-12
 sudo update-alternatives --remove-all clang
 sudo update-alternatives --remove-all clang++
@@ -43,16 +57,11 @@ sudo update-alternatives --remove-all clang-format
 sudo update-alternatives --remove-all clang-tidy
 sudo update-alternatives --force --install /usr/bin/clang clang /usr/bin/clang-15 1500 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-15 --slave /usr/bin/clang-format clang-format /usr/bin/clang-format-15 --slave /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-15
 sudo apt-get update && sudo apt-get install libwebkit2gtk-4.0-dev xvfb -y
-''')
-    os.system(f"export TARGET_ARCH={arch} & xvfb-run script/build.sh build")
-    #listdir()
-    #shutil.move(f"{curr}/webview/build/library/libwebview.so", f"{curr}/platform/{sys.platform}/x86")
-    #os.system(f"export TARGET_ARCH=x64 & xvfb-run script/build.sh build")
-    #shutil.move(f"{curr}/webview/build/library/libwebview.so", f"{curr}/platform/{sys.platform}/x64")
-elif sys.platform=='darwin':
-    os.system(f"export TARGET_ARCH={arch} & script/build.sh build")
-    #listdir()
-    #shutil.move(f"{curr}/webview/build/library/libwebview.dylib", f"{curr}/platform/{sys.platform}/x86")
-    #os.system(f"export TARGET_ARCH=x64 & script/build.sh build")
-    #shutil.move(f"{curr}/webview/build/library/libwebview.dylib", f"{curr}/platform/{sys.platform}/x64")
+"""
+    )
+    os.system("cmake -T host=x64 -B ./build/x64")
+    os.system("cmake --build ./build/x64 --config Release --target ALL_BUILD")
+elif sys.platform == "darwin":
+    os.system("cmake -T host=x64 -B ./build/x64")
+    os.system("cmake --build ./build/x64 --config Release --target ALL_BUILD")
 os.chdir(curr)
